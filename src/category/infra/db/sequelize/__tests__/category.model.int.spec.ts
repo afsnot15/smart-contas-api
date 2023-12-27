4e787f1234be90242b45556f6a761cb1ab4efd2d
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2023-12-26 21:53:21 -0300

    Testes integração sqlite em memoria

--- src/category/infra/db/sequelize/__tests__/category.model.int.spec.ts
+++ src/category/infra/db/sequelize/__tests__/category.model.int.spec.ts
@@ -1,12 +1,13 @@
 
 import { CategoryModel } from "../category.model";
-import { CategoryFakeBuilder } from "../../../../domain/category-fake.builder";
-import { Sequelize } from "sequelize-typescript";
+import { DataType, Sequelize } from "sequelize-typescript";
 import { Category } from "../../../../domain/cateory.entity";
 
 describe("CategoryModel Integration Test", () => {
+    let sequelize;
+
     test("should create a category", async () => {
-        const sequelize = new Sequelize({
+        sequelize = new Sequelize({
             dialect: "sqlite",
             storage: ":memory:",
             models: [CategoryModel],
@@ -21,5 +22,53 @@ describe("CategoryModel Integration Test", () => {
             description: category.description,
             createdAt: category.createdAt,
         });
+    });
+
+    test("mapping props", () => {
+        const attributesMap = CategoryModel.getAttributes();
+        const attributes = Object.keys(CategoryModel.getAttributes());
+        expect(attributes).toStrictEqual([
+            "categoryId",
+            "name",
+            "description",
+            "createdAt",
+        ]);
+
+        const categoryIdAttr = attributesMap.categoryId;
+        expect(categoryIdAttr).toMatchObject({
+            field: "categoryId",
+            fieldName: "categoryId",
+            primaryKey: true,
+            type: DataType.UUID(),
+        });
+
+        const nameAttr = attributesMap.name;
+        expect(nameAttr).toMatchObject({
+            field: "name",
+            fieldName: "name",
+            allowNull: false,
+            type: DataType.STRING(255),
+        });
+
+        const descriptionAttr = attributesMap.description;
+        expect(descriptionAttr).toMatchObject({
+            field: "description",
+            fieldName: "description",
+            allowNull: true,
+            type: DataType.TEXT(),
+        });
+    });
+
+    test("create", async () => {
+        const arrange = {
+            categoryId: "9366b7dc-2d71-4799-b91c-c64adb205104",
+            name: "Category 1",
+            description: "Category 1 description",
+            createdAt: new Date(),
+        };
+
+        const category = await CategoryModel.create(arrange);
+
+        expect(category.toJSON()).toStrictEqual(arrange);
     });
 });
--- src/category/infra/db/sequelize/category.model.ts
+++ src/category/infra/db/sequelize/category.model.ts
@@ -1,26 +1,27 @@
 
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { Column, DataType, Model, PrimaryKey, Table } from "sequelize-typescript";
 
 
 export type CategoryModelProps = {
-    category_id: string;
-    name: string;
-    description: string | null;
-    is_active: boolean;
-    created_at: Date;
-  };
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+};
 
 @Table({ tableName: "categories", timestamps: false })
 export class CategoryModel extends Model {
-    @Column({ type: DataType.UUID })
-    declare categoryId: string;
+  @PrimaryKey
+  @Column({ type: DataType.UUID })
+  declare categoryId: string;
 
-    @Column({ allowNull: false, type: DataType.STRING(255) })
-    declare name: string;
+  @Column({ allowNull: false, type: DataType.STRING(255) })
+  declare name: string;
 
-    @Column({ allowNull: true, type: DataType.TEXT() })
-    declare description: string | null;
+  @Column({ allowNull: true, type: DataType.TEXT() })
+  declare description: string | null;
 
-    @Column({ allowNull: false, type: DataType.DATE(3) })
-    declare createdAt: Date;
+  @Column({ allowNull: false, type: DataType.DATE(3) })
+  declare createdAt: Date;
 }
