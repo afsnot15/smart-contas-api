292bd76508181035aa89bde2d76efd5ce1ceba80
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2024-01-16 18:34:18 +0000

    Caso de uso de pegar uma categoria

---
+++ src/category/application/get-category.use-case.ts
@@ -0,0 +1,37 @@
+import { IUseCase } from "../../shared/application/use-case.interface";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { ICategoryRepository } from "../domain/category.repository";
+import { NotFoundError } from "../../shared/domain/errors/not-found.error";
+import { Category } from "../domain/cateory.entity";
+
+export class GetCategoryUseCase implements IUseCase<GetCategoryInput, GetCategoryOutput>{
+
+    constructor(private readonly categoryRepository: ICategoryRepository) { }
+
+    async execute(input: GetCategoryInput): Promise<GetCategoryOutput> {
+        const uuid = new Uuid(input.id);
+        const category = await this.categoryRepository.findById(uuid);
+
+        if (!category) {
+            throw new NotFoundError(input.id, Category);
+        }
+
+        return {
+            id: category.categoryId.id,
+            name: category.name,
+            description: category.description,
+            createdAt: category.createdAt
+        }
+    }
+}
+
+export type GetCategoryInput = {
+    id: string;
+}
+
+export type GetCategoryOutput = {
+    id: string;
+    name: string;
+    description: string;
+    createdAt: Date;
+}
