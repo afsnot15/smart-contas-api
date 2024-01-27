0fa88e17d848d0cec31abc71f4cecde6059ad635
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2024-01-27 19:35:18 +0000

    Integração do Nest.js com Sequelize

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+
+@Controller('categories')
+export class CategoriesController {
+
+  constructor(private categoryRepo: CategorySequelizeRepository) { 
+    console.log(categoryRepo);
+  }
+
+  @Post()
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+
+  }
+
+  @Get()
+  findAll() {
+
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+
+  }
+}
