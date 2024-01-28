dee1854ec799fee730c33477360d6629187cef71
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2024-01-28 10:32:24 +0000

    Criando módulo para banco de dados

---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { Global, Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+const models = [CategoryModel];
+
+@Module({
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'sqlite' as any,
+            storage: ':memory:',
+            logging: false,
+            models,
+      
+          }),
+    ],
+})
+export class DatabaseModule {}
