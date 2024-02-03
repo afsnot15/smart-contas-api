2766e9562fb86b08cb395eb08b40ca962ea77522
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2024-02-03 14:44:18 -0300

    Usando providers no controller de Categoria

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateCategoryUseCase } from '@core/category/application/use-cases/create-category/create-category.use-case';
+import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update-category/update-category.use-case';
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { DeleteCategoryUseCase } from '@core/category/application/use-cases/delete-category/delete-category.use-case';
+import { GetCategoryUseCase } from '@core/category/application/use-cases/get-category/get-category.use-case';
+import { ListCategoriesUseCase } from '@core/category/application/use-cases/list-categories/list-categories.use-case';
 
 @Controller('categories')
 export class CategoriesController {
 
-  constructor(private categoryRepo: CategorySequelizeRepository) { 
-    console.log(categoryRepo);
-  }
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase;
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase;
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase;
+
+  @Inject(ListCategoriesUseCase)
+  private listteUseCase: ListCategoriesUseCase;
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
